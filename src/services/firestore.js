6de5d31784e1f6db9061d116a9da4b4b5927defb
erichthen <144511616+erichthen@users.noreply.github.com>
2025-03-25 14:37:24 -0400

    30 minute increments, fixed in local host not prod even tho I deployed

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -57,7 +57,7 @@ export const getParentByDocumentId = async (docId) => {
 };
 
 export const getAvailableSlots = async () => {
-  const slots = ["9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM"];
+  const slots = ["9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM"];
   const sessionsSnapshot = await getDocs(collection(db, "sessions"));
   const bookedSlots = sessionsSnapshot.docs.map(doc => doc.data().session_time);
   return slots.filter(slot => !bookedSlots.includes(slot));
